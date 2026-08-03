886f0112cc1f24d5c0cee4e391915b130554ad74
Aditya Garg <aditya.garg@linux.dev>
2026-08-03 19:41:04 +0530

    Move to strscpy
    strncpy has been depreciated for a long time, and finally removed from kernel 7.2
    
    Signed-off-by: Aditya Garg <aditya.garg@linux.dev>

--- t2bce_audio/audio.c
+++ t2bce_audio/audio.c
@@ -441,8 +441,7 @@ static void t2audio_init_dev(struct t2audio_device *a, t2audio_device_id_t dev_i
     INIT_LIST_HEAD(&sdev->list);
     sdev->dev_id = dev_id;
     sdev->buf_id = T2AUDIO_BUFFER_ID_NONE;
-    strncpy(sdev->uid, uid, uid_len);
-    sdev->uid[uid_len + 1] = '\0';
+    strscpy(sdev->uid, uid);
 
     if (t2audio_cmd_get_primitive_property(a, dev_id, dev_id,
             T2AUDIO_PROP(T2AUDIO_PROP_SCOPE_INPUT, T2AUDIO_PROP_LATENCY, 0), NULL, 0, &sdev->in_latency, sizeof(u32)))
